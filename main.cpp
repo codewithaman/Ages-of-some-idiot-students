49e0c4f7e403ed68baa0d990b6e162fb0d44479d
Aman <63802428+codewithaman@users.noreply.github.com>
2021-03-05 14:03:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+
+    cout<<"//////////////////////////EXAM NUMBERS///////////////////////////"<<endl;
+
+    string Name_aman="Aman";
+
+    float marksInEnglish_aman=67.56;
+    float marksInMaths_aman=84.82;
+    float marksInHindi_aman=56.23;
+
+    cout<<"Student = "+Name_aman<<endl;
+
+    cout<<"English = ";
+    cout<<marksInEnglish_aman<<endl;
+    cout<<"Maths = ";
+    cout<<marksInMaths_aman<<endl;
+    cout<<"Hindi = ";
+    cout<<marksInHindi_aman<<endl;
+
+    cout<<endl;
+    cout<<endl;
+
+
+    // Raju
+
+    string Name_raju="Raju";
+
+    float marksInEnglish_raju=53.56;
+    float marksInMaths_raju=76.23;
+    float marksInHindi_raju=97.78;
+
+    cout<<"Student = "+Name_raju<<endl;
+
+    cout<<"English = ";
+    cout<<marksInEnglish_raju<<endl;
+    cout<<"Maths = ";
+    cout<<marksInMaths_raju<<endl;
+    cout<<"Hindi = ";
+    cout<<marksInHindi_raju<<endl;
+
+    return 0;
+}
